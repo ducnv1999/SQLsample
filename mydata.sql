e826b133267d86c0510f86837c9abeb5d0829e74
admin <ducgau99@gmail.com>
2019-04-09 04:36:03 +0700

    first-commit

---
+++ mydata.sql
@@ -0,0 +1,21 @@
+CREATE TABLE contacts
+( contact_id INT(11) NOT NULL AUTO_INCREMENT,
+  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(25),
+  birthday DATE,
+  CONSTRAINT contacts_pk PRIMARY KEY (contact_id)
+);
+select * from contacts;
+CREATE TABLE suppliers
+( supplier_id INT(11) NOT NULL AUTO_INCREMENT,
+  supplier_name VARCHAR(50) NOT NULL,
+  account_rep VARCHAR(30) NOT NULL DEFAULT 'TBD',
+  CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id)
+);
+select * from suppliers;
+drop table contacts;
+ALTER TABLE contacts
+  ADD email varchar(40) NOT NULL
+    AFTER birthday;
+ALTER TABLE contacts
+  MODIFY email varchar(50) NULL;
